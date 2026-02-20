bd6722f3f4e35cff5c3e90d2e84f922abdd62bf2
RobertasKirkickas <robertas3000@hotmail.com>
2026-02-20 11:47:51 +0000

    Collections details api and details page

--- photo-lens/src/navigators/Collections.js
+++ photo-lens/src/navigators/Collections.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import CollectionsScreen from '../screens/Collections';
+import CollectionDetailsScreen from '../screens/CollectionDetails';
+import PhotoDetailsScreen from '../screens/PhotoDetails';
+
 export default function CollectionsNavigator() {
 	const Stack = createStackNavigator();
 	return (
 		<Stack.Navigator style={styles.CollectionsScreen}>
 			<Stack.Screen name='CollectionsScreen' component={CollectionsScreen} options={{ headerShown: false }} />
+			<Stack.Screen name='Collection Details' component={CollectionDetailsScreen} />
+			<Stack.Screen name='Photo Details' component={PhotoDetailsScreen} />
 		</Stack.Navigator>
 	);
 }
